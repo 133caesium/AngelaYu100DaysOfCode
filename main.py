6c1ab7debc9b363ab885d3256a325853f2257266
nber106 <nber106@aucklanduni.ac.nz>
2021-08-11 15:13:39 +1200

    peek into Day054 for Flask

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-
-def sum_digits(twoDigitNumber):
-    if type(twoDigitNumber) != int:
-        raise AttributeError("I need a number to work!")
-    if (twoDigitNumber>99 or twoDigitNumber<10):
-        raise ValueError("The number is too big or too small!")
-    first_digit = int(str(twoDigitNumber)[0])
-    second_digit = int(str(twoDigitNumber)[1])
-    sum_of_digits = first_digit+second_digit
-    return sum_of_digits
-
-def body_mass_index_calculator():
-    height = float(input("enter your height in m:   "))
-    weight = int(input("enter your weight in kg:  "))
-    BMI = round((weight/height**2))
-    print("Your BMI is "+str(BMI))
-    return BMI
+import hello
 
 if __name__ == '__main__':
-    print(sum_digits(10))
-    body_mass_index_calculator()
+    hello.app()
